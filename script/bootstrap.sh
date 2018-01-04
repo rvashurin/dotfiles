87fc495344eb827f6ffd2960b5a33f1f22c075d4
Roman <Roman.Vashurin@interfax.ru>
2018-01-04 23:41:29 +0300

    Start building software bootstrapping script

---
+++ script/bootstrap.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+install_software() {
+
+}
+
+install_yaourt() {
+  git clone https://aur.archlinux.org/package-query.git
+  cd package-query
+  makepkg -si
+  cd ..
+  git clone https://aur.archlinux.org/yaourt.git
+  cd yaourt
+  makepkg -si
+  cd ..
+  rm -rf package-query yaourt
+}
+
+USERNAME=$1
+USERPW=$2
+DOTDIR=$(pwd -P)
+
+echo "Please make sure that members of sudo group are able to execute sudo commands without password! Do you wish to proceed with installation?"
+if [[ read == "no" ]]; then
+  echo "Aborting"
+  exit 0
+fi
+
+useradd $USERNAME
+su $USERNAME -
+
+install_yaourt
+
+sudo pacman -S - < "$DOTDIR/packages/pacman_pkglist.txt"
+yaourt -S - < "$DOTDIR/packages/aur_pkglist.txt" 

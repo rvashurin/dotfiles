af1f5683e6e933689810c76dc904318160b9ef97
Roman <Roman.Vashurin@interfax.ru>
2018-01-13 02:50:49 +0300

    Use zsh as default shell

--- config/zsh/install.sh
+++ config/zsh/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 DIR=$1
+
+chsh -s /bin/zsh
+
 shopt -s dotglob
 
 for link_file in $(ls -a $DIR/*.link); do
@@ -8,4 +11,6 @@ for link_file in $(ls -a $DIR/*.link); do
 done
 
 shopt -u dotglob
+
+ls -sf /usr/share/zsh-theme-powerlevel9k/powerleve9k.zsh-theme /usr/share/oh-my-zsh/themes/powerlevel9k.zsh-theme 
 

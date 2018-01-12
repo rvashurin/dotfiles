52e72c3ea4ede226fd49831d194d84f582d8ea6c
Roman <Roman.Vashurin@interfax.ru>
2018-01-12 22:14:21 +0300

    Add vim plug installation to script

--- config/nvim/install.sh
+++ config/nvim/install.sh
@@ -11,5 +11,8 @@ done
 
 shopt -u dotglob
 
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 nvim +PlugInstall +qall
 

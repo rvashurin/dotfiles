6ed2090725172aa23f2a89b7d201b648dcda5a4e
Roman Vashurin <rvashurin@gmail.com>
2018-10-21 12:28:01 +0300

    Remove ynab, remove noconfirm from aur packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ DOTDIR=$(pwd -P)
 
 sudo pacman -Syy
 
-sudo pacman -S --noconfirm reflector
+sudo pacman -S --noconfirm base-devel reflector
 sudo reflector --latest 100 --sort rate --save /etc/pacman.d/mirrorlist
 
 git clone https://aur.archlinux.org/yay.git
@@ -15,7 +15,8 @@ makepkg -si
 cd ..
 rm -rf yay
 
-yay -S --noconfirm $(cat $DOTDIR/packages/pacman_pkglist.txt $DOTDIR/packages/aur_pkglist.txt | cut -f1 -d' ')
+yay -S --noconfirm $(cat $DOTDIR/packages/pacman_pkglist.txt)
+yay -S $($DOTDIR/packages/aur_pkglist.txt | cut -f1 -d' ')
 
 for dir in $(ls -d $DOTDIR/config/*/); do
   source $dir/install.sh $dir

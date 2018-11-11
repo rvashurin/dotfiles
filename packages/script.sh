c3f3b22b6d67c0886fce46554cb3461bda6b91a3
Roman Vashurin <rvashurin@gmail.com>
2018-11-11 19:27:24 +0300

    Improve packages syncing script

--- packages/script.sh
+++ packages/script.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-echo "Missing native packages:"
-echo $(pacman -Qetnq | grep -v -F -f <(cat pacman_pkglist.txt | cut -f1 -d" ") | grep -v -F -f <(pacman -Qgq base base-devel) | grep -v -F -f <(cat blacklisted_packages.txt))
+BASE_DIR="/home/mhi/workspace/dotfiles/packages"
+MISSING_NATIVE=$(pacman -Qetnq | grep -v -F -f <(cat $BASE_DIR/pacman_pkglist.txt | cut -f1 -d" ") | grep -v -F -f <(pacman -Qgq base base-devel) | grep -v -F -f <(cat $BASE_DIR/blacklisted_packages.txt))
+MISSING_FOREIGN=$(pacman -Qetmq | grep -v -F -f <(cat $BASE_DIR/aur_pkglist.txt | cut -f1 -d" ") | grep -v -F -f <(cat $BASE_DIR/blacklisted_packages.txt))
+
+echo "Missing native packages, adding to db:"
+echo $MISSING_NATIVE
+echo ""
+echo "Missing foreign packages, adding to db:"
+echo $MISSING_FOREIGN
 
 echo ""
-echo "Missing foreign packages:"
-echo $(pacman -Qetmq | grep -v -F -f <(cat aur_pkglist.txt | cut -f1 -d" ") | grep -v -F -f <(cat blacklisted_packages.txt))
+echo "Syncing packages db..."
+cat $/BASE_DIR/pacman_pkglist.txt <($MISSING_NATIVE) | sort -i -o $BASE_DIR/pacman_pkglist.txt
+cat $/BASE_DIR/aur_pkglist.txt <($MISSING_FOREIGN) | sort -i -o $BASE_DIR/aur_pkglist.txt
+echo "OK"
 
 echo ""
 echo "Not installed native packages:"
-echo $(cat pacman_pkglist.txt | cut -f1 -d" " | grep -v -F -f <(pacman -Qeq))
+echo $(cat $BASE_DIR/pacman_pkglist.txt | cut -f1 -d" " | grep -v -F -f <(pacman -Qeq))
 
 echo ""
 echo "Not installed foreign packages:"
-echo $(cat aur_pkglist.txt | cut -f1 -d" " | grep -v -F -f <(pacman -Qeq))
+echo $(cat $BASE_DIR/aur_pkglist.txt | cut -f1 -d" " | grep -v -F -f <(pacman -Qeq))

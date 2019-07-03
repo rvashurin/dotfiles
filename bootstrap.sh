56da2becf02762c57216b788fc8c975cd293e53a
Roman Vashurin <mail+github@vashurin.me>
2019-07-03 22:19:00 +0300

    Various updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,19 +4,19 @@ set -e
 
 DOTDIR=$(pwd -P)
 
-sudo pacman -Syy
-
-sudo pacman -S --noconfirm base-devel reflector
-sudo reflector --latest 100 --sort rate --save /etc/pacman.d/mirrorlist
-
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-rm -rf yay
-
-yay -S --noconfirm $(cat $DOTDIR/packages/pacman_pkglist.txt | cut -f1 -d' ')
-yay -S $(cat $DOTDIR/packages/aur_pkglist.txt | cut -f1 -d' ')
+# sudo pacman -Syy
+# 
+# sudo pacman -S --noconfirm base-devel reflector
+# sudo reflector --latest 100 --sort rate --save /etc/pacman.d/mirrorlist
+# 
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si
+# cd ..
+# rm -rf yay
+# 
+# yay -S --noconfirm $(cat $DOTDIR/packages/pacman_pkglist.txt | cut -f1 -d' ')
+# yay -S $(cat $DOTDIR/packages/aur_pkglist.txt | cut -f1 -d' ')
 
 for dir in $(ls -d $DOTDIR/config/*/); do
   source $dir/install.sh $dir
--- config/pia/install.sh
+++ config/pia/install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
-DIR=$1
-
-shopt -s dotglob
-
-for link_file in $(ls -a $DIR/*.link); do
-  filename = $(basename $link_file | sed s/.link//)
-  sudo ln -sf $link_file /etc/openvpn/client/$(filename);
-  service_name = $(echo "$filename" | sed s/.conf//)
-  sudo systemctl enable "openvpn@$service_name"
-done
-
-shopt -u dotglob
+#
+#DIR=$1
+#
+#shopt -s dotglob
+#
+#for link_file in $(ls -a $DIR/*.link); do
+#  filename=$(basename $link_file | sed s/.link//)
+#  sudo ln -sf $link_file /etc/openvpn/client/$filename;
+#  service_name=$(echo "$filename" | sed s/.conf//)
+#  sudo systemctl enable "openvpn@$service_name"
+#done
+#
+#shopt -u dotglob
 
